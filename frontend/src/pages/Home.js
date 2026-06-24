e00461516f585e92d20ba1de497a2e9efc368278
TasnimC233465 <hurejannattasnim@gmail.com>
2026-06-24 08:33:24 +0600

    Resolve backend merge conflicts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-// Home.js - Full homepage matching design screenshot
+// Home.js 
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { handleSuccess } from '../utils';
